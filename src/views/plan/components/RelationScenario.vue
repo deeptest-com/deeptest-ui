7f45766b23674bdce12dcde2cf72585751637010
wangzhen <wangzhen@nancal.com>
2024-01-12 09:12:51 +0800

    feat:#修复排序bug

--- src/views/plan/components/RelationScenario.vue
+++ src/views/plan/components/RelationScenario.vue
@@ -32,7 +32,7 @@ const props = defineProps<{
 const store = useStore<{ Plan: PlanStateType }>();
 const emits = defineEmits(['onCancel', 'onOk']);
 const scenarioLists = computed<any[]>(() => store.state.Plan.scenarios.list);
-const currPlan = computed<any>(() => store.state.Plan.detailResult);
+const currPlan = computed<any>(() => store.state.Plan.currPlan);
 let pagination = computed<any>(() => store.state.Plan.scenarios.pagination);
 let queryParams = reactive<any>({
   keywords: '', enabled: '1',
@@ -85,6 +85,7 @@ async function onOk() {
 }
 
 async function getScenarioList(params) {
+    console.error(queryParams)
     loading.value = true;
     await store.dispatch('Plan/getScenarioList', {
         ...queryParams,
