5ed4003f77a5cae4ab6c3542eedf2b3d5dc935d4
chenqi99 <chenqi99@nancal.com>
2024-01-30 17:03:48 +0800

    save localValue in browser cache

--- src/views/project-settings/components/EnvSetting/GlobalVar.vue
+++ src/views/project-settings/components/EnvSetting/GlobalVar.vue
@@ -21,20 +21,32 @@
                                 }" :value="text" placeholder="请输入变量名" />
                             </a-form-item>
                         </template>
-                        <template #customLocalValue="{ text, index }">
+
+                        <template #customLocalTitle>
+                          私有值
+                          <Tips title="仅存放在用户本地，不会同步到服务器，也不会在项目成员之间共享，适合存放token、账号密码等敏感数据。注意：清除浏览器/文件缓存时会把变量的本地值清除。"
+                                classes="dp-tip-wide" />
+                        </template>
+                        <template #customLocalValue="{ record, index }">
                             <a-form-item :name="['globalVarsData', index, 'localValue']" :rules="rules['localValue']">
-                                <a-input :value="text" @change="(e) => {
+                                <a-input v-model:value="localValueMap[record.name]" @change="(e) => {
                                     handleGlobalVarsChange('localValue', index, e);
-                                }" placeholder="请输入本地值" />
+                                }" placeholder="请输入私有值" />
                             </a-form-item>
+                        </template>
+
+                        <template #customRemoteTitle>
+                          共享值
+                          <Tips title="保存在远端服务器中，整个项目内成员之间共享数据值。" />
                         </template>
                         <template #customRemoteValue="{ text, index }">
                             <a-form-item :name="['globalVarsData', index, 'remoteValue']" :rules="rules['remoteValue']">
                                 <a-input :value="text" @change="(e) => {
                                     handleGlobalVarsChange('remoteValue', index, e);
-                                }" placeholder="请输入远程值" />
+                                }" placeholder="请输入共享值" />
                             </a-form-item>
                         </template>
+
                         <template #customDescription="{ text, index }">
                             <a-input :value="text" @change="(e) => {
                                 handleGlobalVarsChange('description', index, e);
@@ -70,10 +82,12 @@ import {message, Modal, notification} from "ant-design-vue";
 import { ExclamationCircleOutlined, PlusOutlined } from "@ant-design/icons-vue";
 import EmptyCom from "@/components/TableEmpty/index.vue";
 import PermissionButton from "@/components/PermissionButton/index.vue";
+import Tips from "@/components/Tips/index.vue";
 import { globalVarsColumns } from '../../config';
 import { StateType as ProjectStateType } from "@/store/project";
 import { StateType as ProjectSettingStateType } from "./../../store";
 import {notifyError} from "@/utils/notify";
+import {loadProjectEnvVars, setProjectEnvVar} from "@/utils/cache";
 
 // store 相关
 const store = useStore<{ ProjectGlobal: ProjectStateType, ProjectSetting: ProjectSettingStateType }>();
@@ -85,18 +99,18 @@ const formState = reactive({
 })
 
 const rules = {
-    name: [{
-        required: true,
-        message: '变量名不可为空'
-    }],
-    localValue: [{
-        required: true,
-        message: '本地值不可为空'
-    }],
-    remoteValue: [{
-        required: true,
-        message: '远程值不可为空'
-    }]
+  name: [{
+      required: true,
+      message: '变量名不可为空'
+  }],
+  remoteValue: [{
+      required: true,
+      message: '共享值不可为空'
+  }],
+  // localValue: [{
+  //   required: true,
+  //   message: '私有值不可为空'
+  // }],
 };
 const formRef = ref<any>();
 
@@ -109,12 +123,18 @@ function addGlobalVar() {
 }
 
 async function handleSaveGlobalVars() {
-  // debugger;
     try {
         await formRef.value.validateFields();
+
+        globalVarsData.value.forEach((item, index) => {
+          console.log('setProjectEnvVar', item)
+          setProjectEnvVar(item.projectId, 0, item.name, localValueMap.value[item.name])
+        })
+
         await store.dispatch('ProjectSetting/saveGlobalVars');
-    } catch (err) {
-      notifyError('全局变量名/远程值/本地值不可为空');
+
+    } catch (ex) {
+      console.log(ex);
     }
 }
 
@@ -133,16 +153,19 @@ function handleGlobalVarsChange(field: string, index: number, e: any, action?: s
     }
 }
 
-watch(() => {
-    return currProject.value;
-}, (val: any) => {
-    if (val.id) {
-        getGloablVarsList();
-    }
-}, {
-    immediate: true
-})
+const localValueMap = ref({0: {}})
+watch(() => {return currProject.value;}, async (val: any) => {
+  console.log('watch currProject', currProject.value.id)
+  if (!val.id) return
+
+  await getGloablVarsList();
+
+  localValueMap.value = (await loadProjectEnvVars(currProject.value.id))[0]
+  console.log('localValueMap.value', localValueMap.value)
+}, {immediate: true})
+
 </script>
+
 <style lang="less" scoped>
 .title {
     font-weight: bold;
