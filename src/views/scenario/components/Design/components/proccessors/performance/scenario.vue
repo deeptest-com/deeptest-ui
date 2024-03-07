2748c0ac4712f4a738193c2521f31243c0900b83
chenqi99 <chenqi99@nancal.com>
2024-03-07 09:09:06 +0800

    maintain performance scenario stages

--- src/views/scenario/components/Design/components/proccessors/performance/scenario.vue
+++ src/views/scenario/components/Design/components/proccessors/performance/scenario.vue
@@ -26,8 +26,9 @@
           <div class="dp-param-grid">
             <div class="head">
               <a-row type="flex">
+                <a-col flex="100px" class="title">阶段</a-col>
                 <a-col flex="1" class="title">时长(秒)</a-col>
-                <a-col flex="1" class="title">加载数量</a-col>
+                <a-col flex="1" class="title">虚拟用户数</a-col>
 
                 <a-col flex="80px" class="dp-right">
                   <Tips section="performance-stage" title="维护虚拟用户加载阶段" />
@@ -42,12 +43,18 @@
 
             <div class="params">
               <a-row v-for="(item, idx) in modelRef.stages" :key="idx" type="flex" class="param">
-                <a-col flex="1">
-                  {{ modelRef.duration }}
+                <a-col flex="100px" class="text">
+                  {{ idx + 1 }}
                 </a-col>
 
                 <a-col flex="1">
-                  {{ modelRef.target }}
+                  <a-input v-model:value="item.duration"
+                           class="dp-bg-input-transparent" />
+                </a-col>
+
+                <a-col flex="1">
+                  <a-input v-model:value="item.target"
+                           class="dp-bg-input-transparent" />
                 </a-col>
 
                 <a-col flex="80px" class="dp-right dp-icon-btn-container">
@@ -125,19 +132,20 @@ import {CheckpointType, PerformanceGenerateType, PerformanceGoalType} from "@/ut
 import {requestHeaderOptions} from "@/config/constant";
 import {CheckCircleOutlined, CloseCircleOutlined, DeleteOutlined, PlusOutlined} from "@ant-design/icons-vue";
 import Tips from "@/components/Tips/index.vue";
+import {Header} from "@/views/component/debug/data";
 const useForm = Form.useForm;
 
 const store = useStore<{ Scenario: ScenarioStateType; }>()
 const modelRef: any = computed<boolean>(() => store.state.Scenario.nodeData)
 
 const targetRequired = [{ required: true, message: '请输入虚拟用户数', trigger: 'blur' }]
-const durationRequired = [{ required: true, message: '请输入执行时长', trigger: 'blur' }]
-const loopRequired = [{ required: true, message: '请输入执行轮次', trigger: 'blur' }]
-const responseTimeRequired = [{ required: true, message: '请输入响应时间阀值', trigger: 'blur' }]
-const qpsRequired = [{ required: true, message: '请输入QPS阀值', trigger: 'blur' }]
-const failRateRequired = [{ required: true, message: '请输入错误率阀值', trigger: 'blur' }]
+const durationRequired = [{ required: true, type: 'integer', message: '请输入执行时长', trigger: 'blur' }]
+const loopRequired = [{ required: true, type: 'integer', message: '请输入执行轮次', trigger: 'blur' }]
+const responseTimeRequired = [{ required: true, type: 'number', message: '请输入响应时间阀值', trigger: 'blur' }]
+const qpsRequired = [{ required: true, type: 'number', message: '请输入QPS阀值', trigger: 'blur' }]
+const failRateRequired = [{ required: true, type: 'number', message: '请输入错误率阀值', trigger: 'blur' }]
 
-const rulesRef = reactive({
+const rulesRef = ref({
   generateType: [
     {required: true, message: '请选择加压方式', trigger: 'change'},
   ],
@@ -167,8 +175,17 @@ const goalTypes = ref([
   {label: '失败率', value: PerformanceGoalType.FailRate}])
 
 const submit = debounce(async () => {
+  console.log('submit')
   validate()
       .then(async () => {
+        const stages = [] as any[]
+       modelRef.value.stages.forEach((item) => {
+          if (item.duration && item.target) {
+            stages.push({duration: +item.duration, target: +item.target})
+          }
+        });
+        modelRef.value.stages = stages
+
         const res = await store.dispatch('Scenario/saveProcessor', modelRef.value);
         if (res === true) {
           notifySuccess('保存成功');
@@ -185,14 +202,17 @@ const reset = () => {
   resetFields();
 };
 
-const addStage = (idx) => {
+const addStage = () => {
   console.log('addStage')
+  modelRef.value.stages.push({})
 }
 const insertStage = (idx) => {
   console.log('insertStage')
+  modelRef.value.stages.splice(idx+1, 0, {})
 }
 const removeStage = (idx) => {
   console.log('removeStage')
+  modelRef.value.stages.splice(idx, 1)
 }
 
 onMounted(() => {
@@ -217,7 +237,7 @@ onMounted(() => {
   .stages {
     padding: 0 16px;
     .dp-param-grid {
-      .ant-col {
+      .ant-col.text {
         padding: 0 10px;
         line-height: 32px;
       }
