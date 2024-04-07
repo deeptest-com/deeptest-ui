c32931bb9eaab57f8a1a4cf1c1909f44caa8af0b
yaoxiaofei <yaoxf@nancal.com>
2024-04-07 11:34:41 +0800

    feat: #LYAPI-1375,测试场景用例侧边栏收起样式调整

---
+++ src/views/component/ContentPane/ExpandCollapseSvg.tsx
@@ -0,0 +1,16 @@
+export const ExpandSvg = () => {
+  return (
+    <svg width="14" height="33" viewBox="0 0 14 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0H12C13.1046 0 14 0.895431 14 2V31C14 32.1046 13.1046 33 12 33H0V0Z" />
+      <path d="M9.58685 17.0009L4.55397 12.3787L4.55397 21.623L9.58685 17.0009Z" fill="#2F5CD6" />
+    </svg>
+  );
+};
+export const CollapseSvg = () => {
+  return (
+    <svg width="14" height="33" viewBox="0 0 14 33" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path d="M0 0H12C13.1046 0 14 0.895431 14 2V31C14 32.1046 13.1046 33 12 33H0V0Z" />
+      <path d="M3.55396 17.0009L8.58684 12.3787L8.58684 21.623L3.55396 17.0009Z" fill="#2F5CD6" />
+    </svg>
+  );
+};
