fce2f51f09cd4d0bf1ab8d242781e7f1cfe06c3d
yaoxiaofei <yaoxf@nancal.com>
2024-04-22 15:56:06 +0800

    feat: 增加无代理时,跳转代理设置页面

--- src/components/ExecBtn/index.vue
+++ src/components/ExecBtn/index.vue
@@ -1,7 +1,8 @@
 <template>
   <a-tooltip>
     <template #title>
-      <span v-if="!getCurrAgentUrl">无可用代理,请通过<router-link :to="to">代理设置</router-link>页面安装配置后使用 </span>
+      <span v-if="!getCurrAgentUrl">无可用代理,请通过
+        <span style="cursor: pointer;color: #447DFD" @click="handleToAgentPage">代理设置</span>页面安装配置后使用 </span>
     </template>
     <slot name="execBtn" :isNotClickable="!getCurrAgentUrl"></slot>
   </a-tooltip>
@@ -9,11 +10,24 @@
 <script setup lang="ts">
 import { getAgentUrl } from "@/utils/agentEnv";
 import { computedAsync } from "@vueuse/core";
+import {useWujie} from "@/composables/useWujie";
+import {useRouter} from "vue-router";
 
+const { isInLeyanWujieContainer, parentOrigin } = useWujie();
+const router = useRouter();
 const getCurrAgentUrl = computedAsync(async() => {
   const agentUrl = await getAgentUrl() || null;
   return agentUrl;
 });
 
 const to = 'https://www.baidu.com';
+
+const handleToAgentPage = () => {
+  if (isInLeyanWujieContainer) {
+    const url = `${parentOrigin}/setting/API`;
+    window.open(url, '_blank');
+    return;
+  }
+  window.open(`${window.location.origin}/sys-setting/agent`, '_blank');
+}
 </script>
